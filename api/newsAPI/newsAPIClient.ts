e16177efcbb91b6f14ddce89756f93aa844cd53b
Aneesh Vempaty <aneesh.vempaty@gmail.com>
2025-01-09 10:40:30 -0800

    Reworked to make a scheduled job for 9am news daily

--- api/newsAPI/newsAPIClient.ts
+++ api/newsAPI/newsAPIClient.ts
@@ -1,47 +1,158 @@
 import dotenv from "dotenv";
-
+import { CronJob } from "cron";
+import {
+    Client,
+    TextChannel,
+    REST,
+    Routes,
+    GatewayIntentBits,
+    SlashCommandBuilder,
+} from "discord.js";
 dotenv.config();
 
 export type hackerNews = {
-    title: string,
-    url: string,
-}
+    title: string;
+    url: string;
+};
 
-export async function fetchNewsAPI(): Promise<hackerNews[]> {
-    var date = new Date();
-    var yesterday = date.getDate() - 1;
-    var yesterdayDate = new Date(date.setDate(yesterday));
-    var dateString = yesterdayDate.toISOString().split("T")[0];
-    let response;
-    try {
-        response = await fetch(
-            `https://newsapi.org/v2/everything?apiKey=${process.env.NEWS_API_KEY}&domains=thehackernews.com&from=${dateString}`
+interface NewsChannel {
+    channelId: string;
+    guildName: string;
+    channelName: string;
+}
+const rest = new REST({ version: "9" }).setToken(
+    process.env.DISCORD_BOT_TOKEN!
+);
+
+export default class NewsBot {
+    private client: Client;
+    private job: CronJob;
+    private channel_id_test_channel = "1326033663472963584";
+    private channel_id_tech_news = "1326981935716765878";
+
+    constructor(token: string) {
+        this.client = new Client({
+            intents: ["Guilds", "GuildMessages", "DirectMessages"],
+        });
+
+        this.initializeBot(token);
+
+        this.job = new CronJob(
+            "0 0 9 * * *",
+            () => this.sendNewsToAllChannels(),
+            null,
+            true,
+            "America/Los_Angeles"
         );
-        if (response.ok) {
-            try {
-                let data = await response.json();
-                let list =  processData(data);
-                return list ? list : [];
-            } catch (error) {
-                throw new Error("Error parsing news API response");
-            }
-        } else {
-            throw new Error("Errow fetching news API")
-        }
-    } catch (error) {
-        throw new Error("Error fetching news API");
     }
-}
 
-function processData(data): hackerNews[] {
-    const articles = data.articles;
-    let list: hackerNews[] = [];
-    articles.forEach((article) => {
-        list.push({
-            title: article.title,
-            url: article.url,
-        })
-    })
-    //console.log(list);
-    return list;
+    private async initializeBot(token: string): Promise<void> {
+        this.client.once("ready", () => {
+            console.log(`Logged in as ${this.client.user?.tag}`);
+            this.job.start();
+        });
+
+        await this.client.login(token);
+    }
+
+    private async sendNewsToAllChannels(): Promise<void> {
+        try {
+            const news = await this.fetchNews();
+            const channel1 = await this.client.channels.fetch(
+                this.channel_id_test_channel
+            );
+            if (channel1 instanceof TextChannel) {
+                await channel1.send(news);
+            }
+
+            const channel2 = await this.client.channels.fetch(
+                this.channel_id_tech_news
+            );
+            if (channel2 instanceof TextChannel) {
+                await channel2.send(news);
+            }
+        } catch (err) {
+            console.log(`Error sending news to channels: ${err}`);
+        }
+    }
+
+    private async fetchNews(): Promise<string> {
+        var date = new Date();
+        var yesterday = date.getDate() - 1;
+        var yesterdayDate = new Date(date.setDate(yesterday));
+        var dateString = yesterdayDate.toISOString().split("T")[0];
+        let response;
+        try {
+            response = await fetch(
+                `https://newsapi.org/v2/everything?apiKey=${process.env.NEWS_API_KEY}&domains=thehackernews.com&from=${dateString}`
+            );
+            if (response.ok) {
+                try {
+                    let data = await response.json();
+                    let list = this.processData(data);
+                    return list ? list : "No news for today";
+                } catch (error) {
+                    throw new Error("Error parsing news API response");
+                }
+            } else {
+                throw new Error("Errow fetching news API");
+            }
+        } catch (error) {
+            throw new Error("Error fetching news API");
+        }
+    }
+
+    processData(data): string {
+        var newsListString = "List of Hacker News Articles for today:\n";
+        const articles = data.articles;
+        let list: hackerNews[] = [];
+        articles.forEach((article) => {
+            list.push({
+                title: article.title,
+                url: article.url,
+            });
+        });
+        //console.log(list);
+        newsListString =
+            newsListString +
+            list.map((news) => `**${news.title}**\n${news.url}\n\n`).join("");
+        console.log(newsListString);
+        return newsListString;
+    }
+
+    public async setupCommands(): Promise<void> {
+        this.client.on("interactionCreate", async (interaction) => {
+            if (!interaction.isCommand()) return;
+
+            switch (interaction.commandName) {
+                case "ping":
+                    await interaction.reply("pong");
+                    break;
+                case "hello":
+                    await interaction.reply(
+                        `Hello ${interaction.user.username}`
+                    );
+                    break;
+                case "news":
+                    try {
+                        let newsString = await this.fetchNews();
+                        await interaction.reply(newsString);
+                    } catch (err) {
+                        await interaction.reply("Error fetching news");
+                    }
+            }
+        });
+    }
+
+    public async deployCommands(commands: SlashCommandBuilder[]) {
+        try {
+            await rest.put(
+                Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!),
+                { body: commands }
+            );
+            console.log("Successfully registered application commands");
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
--- index.ts
+++ index.ts
@@ -6,7 +6,8 @@ import {
     RESTPostAPIApplicationCommandsJSONBody,
     GatewayIntentBits,
 } from "discord.js";
-import { fetchNewsAPI, hackerNews } from "./api/newsAPI/newsAPIClient";
+import NewsBot from "./api/newsAPI/newsAPIClient";
+import { hackerNews } from "./api/newsAPI/newsAPIClient";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -29,60 +30,7 @@ const commands = [
         .setDescription("Get the latest news from The Hacker News"),
 ];
 
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.DirectMessages,
-    ],
-});
 
-client.once("ready", async (e) => {
-    console.log(`${e.user.username} is ready!`);
-    console.log(`Bot is in ${e.guilds.cache.size} servers`);
-    newsList = await fetchNewsAPI();
-    console.log(newsList)
-    newsListString = newsListString + newsList.map((news) => `**${news.title}**\n${news.url}\n\n`).join("")
-    console.log(newsListString)
-});
+const newsBot = new NewsBot(process.env.DISCORD_BOT_TOKEN!)
 
-async function deployCommands() {
-    try {
-        await rest.put(
-            Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!),
-            { body: commands }
-        );
-        console.log("Successfully registered application commands");
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
-
-    switch (interaction.commandName) {
-        case "ping":
-            console.log(newsList)
-            await interaction.reply("pong");
-            break;
-        case "hello":
-            await interaction.reply(`Hello ${interaction.user.username}`);
-            break;
-        case "news":
-            await interaction.reply(`${newsListString}`);
-            break;
-    }
-});
-
-// client.on("messageCreate", (message) => {
-//     console.log(message.content);
-//     if (message.author.bot) return;
-
-//     if (message.content === "!ping") {
-//         message.reply('its pong time')
-//         message.reply("pong");
-//     }
-// });
-client.login(process.env.DISCORD_BOT_TOKEN);
-deployCommands();
+newsBot.deployCommands(commands);
